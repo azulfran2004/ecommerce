483ec1a7c63d3eb9c0376b24bbf0eccafc93510d
azulfran2004 <franciscobartolomegarcia@gmail.com>
2024-01-23 20:48:46 +0100

    pdf11

--- app/Http/Livewire/AddCartItemSize.php
+++ app/Http/Livewire/AddCartItemSize.php
@@ -3,9 +3,33 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Size;
 
 class AddCartItemSize extends Component
 {
+    public $product;
+    public $sizes;
+    public $size_id = '';
+    public $colors = [];
+    public $qty = 1;
+    public $quantity = 0;
+    public function mount()
+    {
+        $this->sizes = $this->product->sizes;
+    }
+    public function updatedSizeId($value)
+    {
+        $size = Size::find($value);
+        $this->colors = $size->colors;
+    }
+    public function decrement()
+    {
+        $this->qty--;
+    }
+    public function increment()
+    {
+        $this->qty++;
+    }
     public function render()
     {
         return view('livewire.add-cart-item-size');
