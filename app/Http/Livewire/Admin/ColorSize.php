f401f067baefab4437f050da578a632be08d3b8d
azulfran2004 <franciscobartolomegarcia@gmail.com>
2024-02-05 11:46:41 +0100

    pdf 24

---
+++ app/Http/Livewire/Admin/ColorSize.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+use App\Models\Color;
+use App\Models\ColorSize as TbPivot;
+
+class ColorSize extends Component
+{
+    public $size, $colors;
+    public $color_id, $quantity;
+    public $pivot, $open = false;
+    protected $rules = [
+        'color_id' => 'required',
+        'quantity' => 'required|numeric'
+    ];
+    protected $listeners = ['delete'];
+    public function mount()
+    {
+        $this->colors = Color::all();
+    }
+
+    public function render()
+    {
+        $sizeColors = $this->size->colors;
+        return view('livewire.admin.color-size', compact('sizeColors'));
+    }
+    public function save()
+    {
+        $this->validate();
+        $pivot = TbPivot::where('color_id', $this->color_id)
+            ->where('size_id', $this->size->id)
+            ->first();
+        if ($pivot) {
+            $pivot->quantity += $this->quantity;
+            $pivot->save();
+        } else {
+            $this->size->colors()->attach([
+                $this->color_id => [
+                    'quantity' => $this->quantity,
+                ],
+            ]);
+        }
+        $this->reset(['color_id', 'quantity']);
+        $this->emit('saved');
+        $this->size = $this->size->fresh();
+    }
+    public function edit(TbPivot $pivot)
+    {
+        $this->pivot = $pivot;
+    }
+    public function delete(TbPivot $pivot)
+    {
+        $pivot->delete();
+        $this->size = $this->size->fresh();
+    }
+}
--- app/Http/Livewire/Admin/SizeProduct.php
+++ app/Http/Livewire/Admin/SizeProduct.php
@@ -3,11 +3,58 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\Size;
+use App\Models\Color;
 
 class SizeProduct extends Component
 {
+    public $product, $name, $size;
+    public $pivot, $open = false;
+    public $name_edit;
+    protected $listeners = ['delete'];
+    protected $rules = [
+        'name' => 'required'
+    ];
     public function render()
     {
-        return view('livewire.admin.size-product');
+        $sizes = $this->product->sizes;
+        return view('livewire.admin.size-product', compact('sizes'));
+    }
+    public function save()
+    {
+        $this->validate();
+        $size = Size::where('product_id', $this->product->id)
+            ->where('name', $this->name)
+            ->first();
+        if ($size) {
+            $this->emit('errorSize', 'Esa talla ya existe');
+        } else {
+            $this->product->sizes()->create([
+                'name' => $this->name
+            ]);
+        }
+        $this->product = $this->product->fresh();
+        $this->reset('name');
+    }
+    public function edit(Size $size)
+    {
+        $this->open = true;
+        $this->size = $size;
+        $this->name_edit = $size->name;
+    }
+    public function update()
+    {
+        $this->validate([
+            'name_edit' => 'required',
+        ]);
+        $this->size->name = $this->name_edit;
+        $this->size->save();
+        $this->product = $this->product->fresh();
+        $this->open = false;
+    }
+    public function delete(Size $size)
+    {
+        $size->delete();
+        $this->product = $this->product->fresh();
     }
 }
